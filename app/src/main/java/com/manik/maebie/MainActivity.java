58d8345cd39823a034f2dcf7e71ec1381908608a
manroady1 <gmanik1212@gmail.com>
2020-04-25 12:46:08 +0530

    If user already signed in then it come to MainActivity

--- app/src/main/java/com/manik/maebie/MainActivity.java
+++ app/src/main/java/com/manik/maebie/MainActivity.java
@@ -5,22 +5,54 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth  =FirebaseAuth.getInstance();
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(i);
-                finish();
+
             }
         }, 3000);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentuser = firebaseAuth.getCurrentUser();
+
+        if(currentuser == null){
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    Intent registerintent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(registerintent);
+                    finish();
+                }
+            }, 3000);
+        }else {
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    Intent mainintent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(mainintent);
+                    finish();
+                }
+            }, 3000);
+        }
+    }
+
 }
